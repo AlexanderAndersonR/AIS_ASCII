5e2e8a2ef7e1b7d73494cc79bb04f4efd0dacd20
AlexanderAndersonR <80317808+AlexanderAndersonR@users.noreply.github.com>
2021-06-11 14:41:48 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.bit_in_ASCII = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ASCII_in_bit = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Clear_All_TextBox = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 72);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 83);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(359, 72);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 83);
+            this.textBox2.TabIndex = 1;
+            // 
+            // bit_in_ASCII
+            // 
+            this.bit_in_ASCII.Location = new System.Drawing.Point(616, 97);
+            this.bit_in_ASCII.Name = "bit_in_ASCII";
+            this.bit_in_ASCII.Size = new System.Drawing.Size(98, 23);
+            this.bit_in_ASCII.TabIndex = 2;
+            this.bit_in_ASCII.Text = "_6bit_in_ASCII";
+            this.bit_in_ASCII.UseVisualStyleBackColor = true;
+            this.bit_in_ASCII.Click += new System.EventHandler(this.bit_in_ASCII_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Convert 6 bit in ASCII";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Convert ASCII in 6 bit";
+            // 
+            // ASCII_in_bit
+            // 
+            this.ASCII_in_bit.Location = new System.Drawing.Point(616, 243);
+            this.ASCII_in_bit.Name = "ASCII_in_bit";
+            this.ASCII_in_bit.Size = new System.Drawing.Size(98, 23);
+            this.ASCII_in_bit.TabIndex = 6;
+            this.ASCII_in_bit.Text = "ASCII_in_6bit";
+            this.ASCII_in_bit.UseVisualStyleBackColor = true;
+            this.ASCII_in_bit.Click += new System.EventHandler(this.ASCII_in_bit_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(359, 218);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(212, 83);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(48, 218);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(251, 83);
+            this.textBox4.TabIndex = 4;
+            // 
+            // Clear_All_TextBox
+            // 
+            this.Clear_All_TextBox.Location = new System.Drawing.Point(616, 328);
+            this.Clear_All_TextBox.Name = "Clear_All_TextBox";
+            this.Clear_All_TextBox.Size = new System.Drawing.Size(98, 23);
+            this.Clear_All_TextBox.TabIndex = 8;
+            this.Clear_All_TextBox.Text = "Clear All TextBox";
+            this.Clear_All_TextBox.UseVisualStyleBackColor = true;
+            this.Clear_All_TextBox.Click += new System.EventHandler(this.Clear_All_TextBox_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(598, 42);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 369);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Clear_All_TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ASCII_in_bit);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bit_in_ASCII);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Converter ASCII in 6 bit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button bit_in_ASCII;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ASCII_in_bit;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button Clear_All_TextBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MinimumSize = new Size(747, 408);
+            checkBox1_checked();
+        }
+        public static string ASCII_in_6bit(string coded_number)
+        {
+            string decoding_number = "";
+            byte[] ASCIIBytes = Encoding.ASCII.GetBytes(coded_number);
+            foreach (var item in ASCIIBytes)
+            {
+                if (item <= 87 && item >= 48)
+                {
+                    decoding_number = String.Format("{0:d6}", Convert.ToInt32(Convert.ToString((item - 48), 2)));
+                }
+                else if (item <= 119 && item >= 96)
+                {
+                    decoding_number = String.Format("{0:d6}", Convert.ToInt32(Convert.ToString((item - 56), 2)));
+                }
+            }
+            return decoding_number;
+        }
+        public static string _6bit_in_ASCII(string coded_number)
+        {
+            string decoding_number = "";
+            int counter = 0;
+            var result = 0u;
+            for (var i = coded_number.Length - 1; i >= 0; i--)
+            {
+                if (coded_number[i] == '1')
+                {
+                    result += Convert.ToUInt32(Math.Pow(2, counter));
+                }
+                counter++;
+            }
+            if (result <= 39 && result >= 0)
+            {
+                result += 48;
+            }
+            else if (result <= 63 && result >= 40)
+            {
+                result += 56;
+            }
+            byte[] ASCIIBytes = new byte[] { Convert.ToByte(result) };
+            decoding_number = Encoding.ASCII.GetString(ASCIIBytes);
+            return decoding_number;
+        }
+        private void Clear_All_TextBox_Click(object sender, EventArgs e)
+        {
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+        }
+
+        private void ASCII_in_bit_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                char[] charArray = textBox4.Text.ToCharArray();
+                foreach (var item in charArray)
+                {
+                    textBox3.Text += ASCII_in_6bit(Convert.ToString(item)) + " ";
+                }
+            }
+            else
+            {
+                textBox3.Text = ASCII_in_6bit(textBox4.Text);    
+            }
+        }
+
+        private void bit_in_ASCII_Click(object sender, EventArgs e)
+        {
+            double num = 0.0;
+            string symbol_6 = textBox1.Text;
+
+            if (double.TryParse(textBox1.Text, out num))
+            {
+                if (checkBox1.Checked == true)
+                {
+                    string a = symbol_6;
+                    for (int i = 0; i < textBox1.Text.Length; i += 6)
+                    {
+                        try
+                        {
+                        int cointer = (((a.Length / 6) - 1) * 6) > 0 ? (((a.Length / 6) - 1) * 6) : 6;
+                        textBox2.Text += _6bit_in_ASCII(a.Substring(0, cointer));
+                        a = a.Remove(0, cointer);
+                        }
+                        catch (Exception)
+                        {
+                            textBox2.Text = "ERROR";
+                        }
+                     }
+                }
+                else
+                {
+                    textBox2.Text = _6bit_in_ASCII(textBox1.Text);
+                }
+            }
+            else
+            {
+                textBox2.Text = "Only numbers, please!";
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1_checked();
+        }
+        private void checkBox1_checked()
+        {
+            if (checkBox1.Checked == true)
+            {
+                checkBox1.Text = "several symbols";
+                textBox1.MaxLength = 32767;
+                textBox4.MaxLength = 32767;
+            }
+            else
+            {
+                checkBox1.Text = "1 symbol";
+                textBox1.MaxLength = 6;
+                textBox4.MaxLength = 1;
+            }
+        }
+    }
+}
